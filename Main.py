48404fefec2c00db3a433f16d56b77107314b2ee
Hubert Würschinger <61906395+HubertWuerschinger@users.noreply.github.com>
2023-12-05 13:42:31 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,5 +8,25 @@ def main():
     # Display some test text
     st.write("Das ist ein Testtext auf der Hauptseite der Streamlit-App.")
 
+    # Abschnitt für SelectSlider-Elemente
+    st.header("Materialauswahl für Baumaterialien")
+
+    # Variablen und ihre Bereichsgrenzen
+    variables = {
+        "cement": (100, 500),
+        "slag": (0, 200),
+        "flyash": (0, 200),
+        "water": (100, 300),
+        "superplasticizer": (0, 30),
+        "coarseaggregate": (800, 1200),
+        "fineaggregate": (600, 1000),
+        "age": (1, 365)
+    }
+
+    # Erstellung der SelectSlider für jede Variable
+    for var, (min_val, max_val) in variables.items():
+        selected_value = st.select_slider(f"{var.capitalize()} (Einheit)", range(min_val, max_val + 1))
+        st.write(f"Sie haben {selected_value} als Wert für {var} gewählt.")
+
 if __name__ == "__main__":
     main()
