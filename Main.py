1ef2c6911e3ec9d3a7381758232ad14d01fbbf1f
Hubert Würschinger <61906395+HubertWuerschinger@users.noreply.github.com>
2023-12-06 22:48:35 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -63,7 +63,7 @@ def main():
     if st.button("Vorhersage machen"):
         # Vorhersage mit dem Modell machen
         X_scaled = scaler_X*values
-        prediction = model.predict(X_scaled)
+        prediction = model.predict(values)
         # Anzeige der Vorhersage in einem Ausgabefeld
         st.write("Vorhersageergebnis:")
         st.text_area("Ergebnis", f"{prediction}", height=100)
